c8c7c3dc462ce8216c86aea0ca14896361b15cdd
Bayron110 <bayron124z@hotmail.com>
2026-01-15 13:39:27 -0500

    Version documentos 2

--- src/main/java/com/Carreras/Bayron/Backen_Carreras/Controller/DocumentosController.java
+++ src/main/java/com/Carreras/Bayron/Backen_Carreras/Controller/DocumentosController.java
@@ -2,7 +2,15 @@ package com.Carreras.Bayron.Backen_Carreras.Controller;
 
 import com.Carreras.Bayron.Backen_Carreras.Entity.Documento;
 import com.Carreras.Bayron.Backen_Carreras.Services.DocumentoService;
+import com.mongodb.client.gridfs.model.GridFSFile;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.gridfs.GridFsResource;
+import org.springframework.data.mongodb.gridfs.GridFsTemplate;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -17,6 +25,9 @@ public class DocumentosController {
     @Autowired
     private DocumentoService documentoService;
 
+    @Autowired
+    private GridFsTemplate gridFsTemplate;
+
     @PostMapping("/upload")
     public ResponseEntity<String> uploadDocumento(@RequestParam("file") MultipartFile file,
                                                   @RequestParam("nombre") String nombre,
@@ -28,5 +39,20 @@ public class DocumentosController {
     @GetMapping("/list")
     public ResponseEntity<List<Documento>> listDocumentos() {
         return ResponseEntity.ok(documentoService.listDocumentos());
+    }
+
+    // ✅ Nuevo endpoint para descargar usando fileId
+    @GetMapping("/download/{fileId}")
+    public ResponseEntity<Resource> downloadDocumento(@PathVariable String fileId) throws IOException {
+        GridFSFile file = gridFsTemplate.findOne(new Query(Criteria.where("_id").is(new ObjectId(fileId))));
+        if (file == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        GridFsResource resource = gridFsTemplate.getResource(file);
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(file.getMetadata().getString("_contentType")))
+                .body(resource);
     }
 }
